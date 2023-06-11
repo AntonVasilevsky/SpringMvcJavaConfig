756c06709bafc362c5795d685ce985312d251912
AntonVasilevsky <q3antonio@gmail.com>
2023-06-11 12:22:10 +0300

    passed params with @requestparam and with link in html

--- src/main/java/ru/anton/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/anton/springcourse/config/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ru.anton.springcourse.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/first")
 public class FirstController {
-    @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    @GetMapping("/hello")              //opens the page and passes null, true -> 404
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam("surname") String surname){
+
         System.out.println(String.format("Hello %s %s", name, surname));
         return "first/hello";
     }
